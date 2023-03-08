2694b88201e4242f25bf44d09c999fc5ce42beb2
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-03-07 23:08:56 -0300

    add detail page

--- src/components/TeamDetail/TeamDetail.tsx
+++ src/components/TeamDetail/TeamDetail.tsx
@@ -1,24 +1,30 @@
-import React from "react";
-
-import { useParams } from "react-router-dom";
-
-const TeamDetail = () => {
+import { Team } from "@/types/Team";
+import {  useParams } from "react-router-dom";
+import TeamDetailsTable from "@/components/TeamDetailsTable/TeamDetailsTable";
+interface Props {
+  teams?: Team[];
+}
+const TeamDetail = ({ teams }: Props) => {
   const { id } = useParams();
-
+  const team = teams?.find((team) => team.id === id);
+  if (!team) {
+    return <>go back</>
+  }
   return (
-    <div className=" p-8 grid grid-cols-12 gap-4">
-      <div className=" col-span-8  grid grid-cols-8 gap-4">
+    <div className="grid grid-cols-12 gap-4 p-8 ">
+      <div className="grid grid-cols-8 col-span-8 gap-4 ">
         <div className="col-span-8">
-          {/*  <StandingsTable teams={teams} /> */}
+
+           <TeamDetailsTable team={team} />
         </div>
         <div className="col-span-3">
           {/*  <PlayersTable players={players} /> */}
         </div>
-        <div className="col-span-5 h-60 bg-gray-400"></div>
+        <div className="col-span-5 bg-gray-400 h-60"></div>
       </div>
-      <div className=" col-span-4 bg-teal-200 grid grid-cols-4 gap-4 content-start">
-        <div className="col-span-4 h-20 bg-gray-400"></div>
-        <div className="col-span-4 h-20 bg-gray-400"></div>
+      <div className="grid content-start grid-cols-4 col-span-4 gap-4 bg-teal-200 ">
+        <div className="h-20 col-span-4 bg-gray-400"></div>
+        <div className="h-20 col-span-4 bg-gray-400"></div>
       </div>
 
       <div>hola</div>
---
+++ src/components/TeamDetailsTable/TeamDetailsTable.tsx
@@ -0,0 +1,65 @@
+
+import { Team } from '@/types/Team';
+import React from 'react'
+import LastFiveGames from '../LastFiveGames/LastFiveGames';
+import TableCell from '../TableCell/TableCell';
+import TableRow from '../TableRow/TableRow';
+import TableWrapper from '../TableWrapper/TableWrapper';
+
+const TeamDetailsTable = ({team}:{team:Team}) => {
+    const tableHeaders = [
+        { label: "#", classes: "" },
+        { label: "Team", classes: "pl-2 text-left" },
+        { label: "Games", classes: "" },
+        { label: "W", classes: "" },
+        { label: "T", classes: "" },
+        { label: "L", classes: "" },
+        { label: "GF", classes: "" },
+        { label: "GA", classes: "" },
+        { label: "GD", classes: "" },
+        { label: "Pts.", classes: "" },
+        { label: "Last 5", classes: "" },
+      ];
+    const {name,games,wins,loses,ties,goalsFor,goalsAgainst,goalDifference,points, lastFiveGames}=team
+      return (
+        <TableWrapper>
+          <table className="w-full">
+            <thead>
+              <TableRow>
+                {tableHeaders.map((h, idx) => (
+                  <TableCell
+                    key={`${h.label}-${idx}`}
+                    tag="th"
+                    className={h.classes}
+                  >
+                    {h.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </thead>
+            <tbody>
+             
+                    <TableRow >
+                      <TableCell>?</TableCell>
+                      <TableCell className="pl-2 text-left">{name}</TableCell>
+                      <TableCell>{games}</TableCell>
+                      <TableCell>{wins}</TableCell>
+                      <TableCell>{ties}</TableCell>
+                      <TableCell>{loses}</TableCell>
+                      <TableCell>{goalsFor}</TableCell>
+                      <TableCell>{goalsAgainst}</TableCell>
+                      <TableCell>{goalDifference}</TableCell>
+                      <TableCell>{points}</TableCell>
+                      <TableCell>
+                        <LastFiveGames games={lastFiveGames} />
+                      </TableCell>
+                      
+                    </TableRow>
+         
+            </tbody>
+          </table>
+        </TableWrapper>
+      );
+    };
+
+export default TeamDetailsTable

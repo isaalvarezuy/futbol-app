9bc0af9d775b8b4a0cf8aad1e314cce12c2a043f
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-05-14 19:38:24 -0300

    add zod

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import Input from "../Input/Input";
 import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/schemas/login.schema";
 
 const LoginForm = () => {
   const {
     formState: { errors },
-  } = useForm();
+    handleSubmit,
+  } = useForm({ resolver: zodResolver(loginSchema) });
 
+  console.log(errors);
   return (
     <div className="flex flex-col gap-4 p-4">
-      <Input errors={errors} name={"hola"} />
-      <Input errors={errors} name={"pass"} type="password" />
-      <Button>click</Button>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <Input errors={errors} name={"email"} />
+        <Input errors={errors} name={"password"} type="password" />
+        <Button type="submit">click</Button>
+      </form>
     </div>
   );
 };

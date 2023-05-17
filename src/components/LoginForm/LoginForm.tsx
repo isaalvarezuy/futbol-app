32241c3bf78a4b0ae4924f6cf2e9a8463f0e50b9
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-05-17 16:44:55 -0300

    update input component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../Input/Input";
 import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema } from "@/schemas/login.schema";
+import InputNew from "../InputNew/InputNew";
+import { Eye, EyeOff } from "react-feather";
 
 const LoginForm = () => {
   const {
     formState: { errors },
     handleSubmit,
+    register,
   } = useForm({ resolver: zodResolver(loginSchema) });
 
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
   console.log(errors);
   return (
     <div className="flex flex-col gap-4 p-4">
       <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <Input errors={errors} name={"email"} />
-        <Input errors={errors} name={"password"} type="password" />
+        <InputNew
+          label="Email"
+          type="text"
+          error={errors["email"]}
+          {...register("email")}
+        />
+        <InputNew
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          iconRight={
+            !showPassword ? (
+              <Eye onClick={toggleShowPassword} />
+            ) : (
+              <EyeOff onClick={toggleShowPassword} />
+            )
+          }
+          error={errors["password"]}
+          {...register("password")}
+        />
+
         <Button type="submit">click</Button>
       </form>
     </div>
--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email().min(1),
-  password: z.string(),
+  password: z.string().min(1),
 });

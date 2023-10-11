f959b6312daf3118d7a25980ea1d3656e2128af6
isaalvarrez <87995315+isaalvarezuy@users.noreply.github.com>
2023-10-11 11:05:36 -0300

    create temporary type

--- src/components/AddGameForm/AddGameForm.tsx
+++ src/components/AddGameForm/AddGameForm.tsx
@@ -9,9 +9,20 @@ import { Button } from "ia-moonlight";
 import { useEffect } from "react";
 import { getTeamPlayers } from "@/utils/utils";
 import { Team } from "@/types/Team";
+import { Plus, Trash2 } from "react-feather";
+import IconWrapper from "../IconWrapper/IconWrapper";
+
+type TeamType = {
+  team: string;
+  goals: number;
+  goalScorers: {
+    player: string;
+    goals: string;
+  }[];
+};
 
 const AddGameForm = ({ teams }: { teams: Team[] }) => {
-  const { handleSubmit, register, control, setValue } = useForm({
+  const { handleSubmit, register, control, setValue } = useForm<TeamType>({
     defaultValues: {
       team: teams[0].id,
       goals: 0,
@@ -19,7 +30,7 @@ const AddGameForm = ({ teams }: { teams: Team[] }) => {
     },
   });
 
-  const { fields, append } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     control,
     name: "goalScorers", // unique name for your Field Array
   });
@@ -45,10 +56,11 @@ const AddGameForm = ({ teams }: { teams: Team[] }) => {
     if (!teamGoals) setValue("goalScorers", []);
   }, [teamGoals]);
 
-  const canAddGoalScorer = totalGoalsByGoalScorer < teamGoals;
+  const canAddGoalScorer =
+    !totalGoalsByGoalScorer || totalGoalsByGoalScorer < teamGoals;
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)} className="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         {teams && (
           <div className="flex gap-2">
             <Select
@@ -66,19 +78,22 @@ const AddGameForm = ({ teams }: { teams: Team[] }) => {
           </div>
         )}
         {fields.map((field, index) => (
-          <div className="flex gap-2 w-full" key={field.id}>
+          <div className="flex gap-2 w-full items-center" key={field.id}>
             <Select
               options={teamPlayers?.map((player) => ({
                 value: player.id,
                 label: player.name,
               }))}
-                  {...register(`goalScorers.${index}.player`)}
+              {...register(`goalScorers.${index}.player`)}
             />
             <InputNew
               containerClassName="w-16"
               type="number"
-                {...register(`goalScorers.${index}.goals`)}
+              {...register(`goalScorers.${index}.goals`)}
             />
+            <button onClick={() => remove(index)}>
+              <Trash2 className="h-4 text-red-600 hover:text-red-400" />
+            </button>
           </div>
         ))}
         {teamGoals > 1 && (
@@ -87,12 +102,15 @@ const AddGameForm = ({ teams }: { teams: Team[] }) => {
             onClick={() => {
               append({});
             }}
+            className="w-full mt-2"
             disabled={!canAddGoalScorer}
           >
+            <IconWrapper size={16}>
+              <Plus />
+            </IconWrapper>
             Add goal scorer
           </Button>
         )}
-        <button type="submit">click</button>
       </form>
     </div>
   );
